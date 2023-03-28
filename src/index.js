0205fd4fce831e9f9431a7b02e9fdade99ae6ef9
DmitryMass <dimamikron41@gmail.com>
2023-03-28 19:24:03 +0300

    starter pack

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import pkg from 'body-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import errorHandler from './middleware/errorHandler.js';
+
+// config
+const { json, urlencoded } = pkg;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+mongoose.set('strictQuery', true);
+
+const app = express();
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('common'));
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use('/assets', express.static(path.join(__dirname, './assets')));
+// routes
+
+// test route
+app.get('/', (req, res) => {
+    return res.status(200).send({ msg: 'Server OK' });
+});
+
+// error handler
+app.use(errorHandler);
+
+const start = async () => {
+    try {
+        mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        app.listen(process.env.PORT || 5005, () => {
+            console.log(`Server start on port ${process.env.PORT}`);
+        });
+    } catch (err) {
+        console.log(`${err} Error when server start ${process.env.PORT}`);
+    }
+};
+start();
