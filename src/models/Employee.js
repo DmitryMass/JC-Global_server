aab4d210866bf29ba8a046d6b61ed6f49dcff978
DmitryMass <dimamikron41@gmail.com>
2023-03-30 14:55:51 +0300

    merged companyGoals to master / added Employee model

---
+++ src/models/Employee.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from 'mongoose';
+
+const EmployeeSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      max: 70,
+    },
+    name: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 25,
+    },
+    surname: {
+      type: string,
+      required: true,
+      min: 2,
+      max: 30,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    photoPath: {
+      type: String,
+      default: '',
+    },
+    jobTitle: {
+      type: String,
+      required: true,
+      max: 70,
+    },
+    vacation: {
+      type: String,
+      default: '0',
+    },
+    schedule: [
+      {
+        _id: false,
+        id: { type: Schema.Types.ObjectId, required: true, auto: true },
+        day: { type: String, required: true },
+        time: { type: String, required: true },
+        // workedShift: { type: Boolean, default: false },
+        // workDay: { type: String, default: '0' },
+        // workHours: { type: String, default: '10' },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Employee = mongoose.model('Employee', EmployeeSchema);
