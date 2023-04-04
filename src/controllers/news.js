b4d897c074e020ef8d0659fd60399fccc2265ec0
DmitryMass <dimamikron41@gmail.com>
2023-04-04 22:21:41 +0300

    create News with photo files complete

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -9,7 +9,7 @@ export const getNews = async (req, res) => {
     const news = await News.find();
     if (!news) return res.status(404).send({ msg: 'Новостей пока нет.' });
 
-    return res.status(200).send(news);
+    return res.status(200).send(news.sort((a, b) => b.createdAt - a.createdAt));
   } catch (err) {
     return res
       .status(500)
@@ -28,6 +28,24 @@ export const createNews = async (req, res) => {
       });
       await newNews.save();
       const news = await News.find();
+      return res.status(200).send(news);
+    }
+
+    if (!Array.isArray(req.files.files)) {
+      const newFileName = `${Date.now()}-${req.files.files.name}`;
+      const newNews = new News({
+        text,
+        header,
+        imgPath: [newFileName],
+      });
+      await newNews.save();
+      const news = await News.find();
+
+      req.files.files.mv(`${__dirname}/assets/${newFileName}`, (err) => {
+        if (err) {
+          return res.status(400).send(err);
+        }
+      });
       return res.status(200).send(news);
     }
 
