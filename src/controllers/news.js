cd2684710fa28ee2aab135119b592b9aa77bfe0b
DmitryMass <dimamikron41@gmail.com>
2023-04-06 12:51:15 +0300

    change delete Route for News

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -77,10 +77,10 @@ export const createNews = async (req, res) => {
 export const deleteNews = async (req, res) => {
   try {
     const { id } = req.params;
-    const news = await News.findById(id);
+    const news = await News.findByIdAndDelete(id);
     if (!news)
       return res.status(404).send({ msg: 'Такой новости не существует' });
-    await news.remove();
+
     return res.status(200).send({ msg: 'Новость удалена' });
   } catch (err) {
     return res.status(500).send({ msg: 'Ошибка сервера при удалении новости' });
