d255aca7dfd1499178b50a6071fef99ec7967fc5
DmitryMass <dimamikron41@gmail.com>
2023-05-09 17:51:23 +0300

    added basic route for resetPassword (without url accepted link)

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -1,4 +1,4 @@
-import { compare, hash } from 'bcrypt';
+import { compare, hash, genSalt } from 'bcrypt';
 import * as dotenv from 'dotenv';
 dotenv.config();
 import { Employee } from '../models/Employee.js';
@@ -6,6 +6,9 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import Category from '../models/Category.js';
 import pkg from 'jsonwebtoken';
+import { promisify } from 'util';
+import { transporter } from '../nodemailer/nodemailer.js';
+import crypto from 'crypto';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -140,6 +143,7 @@ export const login = async (req, res) => {
     const checkPassword = await compare(password, employee.password);
     if (!checkPassword)
       return res.status(401).send({ msg: 'Введені не вірні данні.' });
+    // Можно закинуть expiris N-hours
     const token = pkg.sign({ id: employee._id }, process.env.SECRET_KEY);
     return res.status(200).send({
       userData: {
@@ -208,5 +212,54 @@ export const editEmployeeData = async (req, res) => {
     return res
       .status(500)
       .send({ msg: 'Помилка серверу при редагуванні даних співробітника.' });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const employee = await Employee.findOne({ email });
+    if (!employee)
+      return res
+        .status(404)
+        .send({ msg: 'Співробітника з такою поштою не знайдено' });
+
+    const generatePassword = (length) => {
+      const chars =
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+      const randomChars = Array.from(
+        crypto.randomFillSync(new Uint32Array(length))
+      ).map((x) => chars[x % chars.length]);
+      return randomChars.join('');
+    };
+
+    const newPassword = generatePassword(12);
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: 'Відновлення паролю.',
+      text: `Ваш новий пароль: ${newPassword}`,
+    };
+
+    transporter.sendMail(mailOptions, async (error) => {
+      if (error) {
+        console.log(error);
+        return res
+          .status(500)
+          .send({ msg: 'Не вдалося відправити електронний лист.' });
+      }
+      await employee.updateOne({
+        password: await hash(newPassword, 5),
+      });
+
+      return res
+        .status(200)
+        .send({ msg: 'Новий пароль успішно відправлено на пошту.' });
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ msg: 'Помилка серверу при відновленні паролю' });
   }
 };
